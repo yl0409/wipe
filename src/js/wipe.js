86edb35dcd790eeaa6de30629b1020592cce4a41
yl0409 <1104413312@qq.com>
2018-11-13 10:08:15 +0800

    增加移动端支持

--- src/js/wipe.js
+++ src/js/wipe.js
@@ -7,7 +7,34 @@
 		context.fillRect(0,0,_w,_h);
 		context.globalCompositeOperation = "destination-out";
 	}
+	function drawarc(context,moveX,moveY){
+	console.log("传递的实参个数"+arguments.length);
+	context.beginPath();
+	context.fillStyle="red";
+	context.arc(moveX,moveY,radius,0,2*Math.PI);
+	context.fill();
+	context.restore();
+}
+	// function draw(context,a,b,c,d){
+	// 	if(arguments.length==3){
+	// 		context.beginPath();
+	// 		context.fillStyle="red";
+	// 		context.arc(a,b,radius,0,2*Math.PI);
+	// 		context.fill();
+	// 		context.restore();
+	// 	}else{
+	// 		context.save();
+	// 		context.beginPath();
+	// 		context.moveTo(a,b);
+	// 		context.lineTo(c,d);
+	// 		context.lineCap = "round";
+	// 		context.lineWidth = radius*2;
+	// 		context.stroke();
+	// 		context.restore();
+	// 	}
+	// }
 	function drawLine(context,one,two,three,four){
+		console.log(arguments.length);
 		context.save();
 		context.beginPath();
 		context.moveTo(one,two);
@@ -22,51 +49,54 @@
 	var deliaY;
 	var isMouseDown = false;
 	//如果true执行鼠标移动事件，鼠标按下等于true,鼠标抬起等于false
-	cas.addEventListener("mousedown",function(evt){
-		var event = evt || window.event;
-		//获取鼠标在视口的坐标，传递参数到drawPoint
-		deliaX = event.clientX;
-		deliaY = event.clientY;
-		drawLine(context,deliaX,deliaY);
-		isMouseDown=true;
-		console.log(isMouseDown);
-	},false);
-		cas.addEventListener("mousemove",function(evt){
-		if(isMouseDown===true){
+
+	var device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+	console.log(navigator.userAgent);
+	console.log(device);
+	// var startEvt,moveEvt,endEvt;
+	// if(device == true){
+	// 	startEvt = "touchstart";
+	// 	moveEvt = "touchmove";
+	// 	endEvt = "touchend";
+	// }else{
+	// 	startEvt = "mousedown";
+	// 	moveEvt = "mousemove";
+	// 	endEvt = "mouseup";
+	// }
+	var startEvt = device ? "touchstart" : "mousedown";
+	var moveEvt = device ? "touchmove" : "mousemove";
+	var endEvt = device ? "touchend" : "mouseup";
+
+	cas.addEventListener(startEvt,function(evt){
+		isMouseDown = true;
+		var event = evt || window.enent;
+		deliaX = device ? event.touches[0].clientX : event.clientX;
+		deliaY = device ? event.touches[0].clientY : event.clientY;
+		drawarc(context,deliaX,deliaY);
+	})
+	cas.addEventListener(moveEvt,function(evt){
+		if(isMouseDown == true){
 			var event = evt || window.event;
-			var moveX = event.clientX;
-			var moveY = event.clientY;
+			event.preventDefault();
+			var moveX = device ? event.touches[0].clientX : event.clientX;
+			var moveY = device ? event.touches[0].clientY : event.clientY;
 			drawLine(context,deliaX,deliaY,moveX,moveY);
 			deliaX=moveX;
 			deliaY=moveY;
-			console.log(isMouseDown);
+		}else{
+			return false;
 		}
-	},false);
-	cas.addEventListener("mouseup",function(evt){
+	})
+	cas.addEventListener(endEvt,function(evt){
 		isMouseDown=false;
 		var num = [];
 		var imgdata = context.getImageData(0,0,cas.width,cas.height);
-		console.log(imgdata);
-		// for(i=0;i<cas.height;i++){
-		// 	for(j=0;j<cas.width;j++){
-		// 		var a = ((cas.width*i)+j)*4+3;
-		// 		if(imgdata.data[a]==0){
-		// 			num.push(a);
-
-		// 		}
-		// 	}
-		// }
-		// var x = (num.length/(cas.width*cas.height))*100;
-		// console.log(x);
-		// 	if(x>=60){
-		// 		context.clearRect(0,0,_w,_h);
-		// 		alert("很抱歉，没中奖！");
-		// 	};
+		// console.log(imgdata);
 		if(getTransparencyPercent(context) >= 60){
 			clear(context);
 			alert("哈哈哈哈");
 		}
-	});
+	})
 	function getTransparencyPercent(context){
 		var t = 0;
 		var imgData = context.getImageData(0,0,_w,_h);
@@ -77,7 +107,6 @@
 			}
 		}
 		var percent = (t / (_w*_h)*100);
-		console.log(percent);
 		return Math.round(percent);
 	}
 	function clear(context){
